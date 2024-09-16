658340477db5a602cadab0a15fd5ca240d9ca069
Snek <daniell5984@gmail.com>
2024-09-16 14:47:03 +0100

    Squashed commit of the following:
    commit 26ad41b18ff2b3e52f8c89aa63c77d00352308d6
    Author: Snek <daniell5984@gmail.com>
    Date:   Mon Sep 16 14:38:43 2024 +0100
    
        aa
    
    commit 8d657a189d955947b5e3401299cbca09d131a4b1
    Author: Snek <daniell5984@gmail.com>
    Date:   Mon Sep 16 14:00:40 2024 +0100
    
        check

--- utils/process_image_API.py
+++ utils/process_image_API.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from PIL import Image, ImageFile
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -34,8 +35,14 @@ class ProcessImageAPI:
         )
         self.progress_lock = threading.Lock()
     
-    def process_image_batch(self, images_batch, folder_path, basefolder, image_paths_batch, counter, lock, augment=None, global_pbar=None):
-        self.process_images(images_batch, folder_path, basefolder, image_paths_batch, counter, lock, rename_output_file=self.rename_output_file, augment=augment, global_pbar=global_pbar)
+    async def process_image_batch(self, images_batch, folder_path, basefolder, image_paths_batch, counter, lock, augment=None, global_pbar=None):
+        tasks = []
+        for image, image_path in zip(images_batch, image_paths_batch):
+            with lock:
+                count = counter.value
+                counter.value += 1
+            tasks.append(self.process_image(image, folder_path, basefolder, image_path, count, lock, augment, global_pbar))
+        await asyncio.gather(*tasks)
     
     def create_tags_file(self, annotation, tags, folder_path, output_path, augment=None):
         import utils.caption_utils as caption_utils
@@ -55,24 +62,36 @@ class ProcessImageAPI:
             content = caption_utils.remove_duplicate_phrases(content)
             caption_utils.write_tags_file(output_path=tags_file_path, content_list=[content])
     
-    def process_image(self, image, folder_path, basefolder, image_path, count, lock, augment=None, global_pbar=None):
+    async def process_image(self, image, folder_path, basefolder, image_path, count, lock, augment=None, global_pbar=None):
         try:
             if not os.path.exists(image_path):
                 logger.warning(f"Image not found: {image_path}")
                 return
-            output_path = os.path.join(basefolder, f"img_{count:03d}" if self.rename_output_file else image['name'])
-            self.create_tags_file(image.get("annotation"), image.get("tags"), folder_path, output_path, augment)
+            
+            if self.rename_output_file:
+                output_filename = f"img_{count:03d}.png"
+            else:
+                output_filename = f"{os.path.splitext(image['name'])[0]}.png"
+
+            output_path = os.path.join(basefolder, output_filename)
+
+            self.create_tags_file(image.get("annotation"), image.get("tags"), folder_path, os.path.splitext(output_path)[0], augment)
 
             if image['ext'].lower() == "svg":
-                self._process_svg(image_path, output_path)
+                result = self._process_svg(image_path, output_path)
             else:
-                self._process_raster(image_path, output_path)
-            
+                result = self._process_raster(image_path, output_path)
+
+            if config.doUpscale:
+                await self.upscale_api.queue_image(img=result, output_path=output_path)
+            else:
+                self._process_without_upscale(result, output_path)
+
             if global_pbar:
                 with self.progress_lock:
                     global_pbar.update(1)
         except Exception as e:
-            logger.error(f"Error in processing image {image_path}: {e}")
+            logger.error(f"Error in processing image {image_path}: {e}", exc_info=True)
     
     def _process_svg(self, image_path, output_path):
         try:
@@ -84,49 +103,32 @@ class ProcessImageAPI:
             return None
     
     def _process_raster(self, image_path, output_path):
-        try:
-            img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-            if img is None:
-                raise ValueError(f"Failed to load image: {image_path}")
+        img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        assert img is not None, logger.error(f"_process_raster: Image not found: {image_path}")
 
-            h, w = img.shape[:2]
-            min_dimension = 10
-            if h < min_dimension or w < min_dimension:
-                raise ValueError(f"Image dimensions too small: {w}x{h}")
+        h, w = img.shape[:2]
 
-            # Correct channel organization
-            if len(img.shape) == 2:
-                img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-            elif img.shape[2] == 3:
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            elif img.shape[2] == 4:
-                img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
-            else:
-                raise ValueError(f"Unexpected number of channels: {img.shape[2]}")
-            
-            # Handle RGBA images or transparent images or mode is L
-            if img.shape[2] == 4 or img.shape[2] == 3 or img.mode == "L":
-                img = image_utils.fill_transparent_with_color_cv2(img, config.padding)
+        # Correct channel organization
+        if len(img.shape) == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        elif img.shape[2] == 3:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        elif img.shape[2] == 4:
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+        else:
+            raise ValueError(f"Unexpected number of channels: {img.shape[2]}")
 
-            # Center square crop (if applicable)
-            if config.do_center_square_crop:
-                size = min(h, w)
-                start_y = (h - size) // 2
-                start_x = (w - size) // 2
-                img = img[start_y:start_y+size, start_x:start_x+size]
+        # Handle RGBA images or transparent images
+        if img.shape[2] == 4 or img.shape[2] == 3:
+            img = image_utils.fill_transparent_with_color_cv2(img, config.padding)
 
-            if config.doUpscale:
-                try:
-                    self.upscale_api.queue_image(img=img, output_path=output_path)
-                except Exception as upscale_error:
-                    print(f"Error in upscaling image {image_path}: {upscale_error}")
-                    print("Falling back to non-upscaled processing.")
-                    self._process_without_upscale(img, output_path)
-            else:
-                self._process_without_upscale(img, output_path)
-        except Exception as e:
-            logger.error(f"Error processing raster image {image_path}: {e}")
-            return None
+        if config.do_center_square_crop:
+            size = min(h, w)
+            start_y = (h - size) // 2
+            start_x = (w - size) // 2
+            img = img[start_y:start_y+size, start_x:start_x+size]
+
+        return img
     
     def _process_without_upscale(self, img, output_path):
         try:
@@ -147,33 +149,6 @@ class ProcessImageAPI:
         except Exception as e:
             logger.error(f"Error in non-upscaled processing for image {output_path}: {e}")
             return None
-    
-    def process_images(self, images, folder_path, basefolder, image_paths, counter, lock, rename_output_file=True, augment=None, global_pbar=None):
-        with ThreadPoolExecutor(max_workers=config.number_of_jobs) as executor:
-            futures = []
-            for image, image_path in zip(images, image_paths):
-                with lock:
-                    count = counter.value
-                    counter.value += 1
-                futures.append(
-                    executor.submit(
-                        self.process_image,
-                        image,
-                        folder_path,
-                        basefolder,
-                        image_path,
-                        count,
-                        lock,
-                        augment,
-                        global_pbar
-                    )
-                )
-        
-            for future in as_completed(futures):
-                try:
-                    future.result()
-                except Exception as e:
-                    logger.error(f"Error processing image: {e}")
     
     def close(self):
         if self.llm_processor:
